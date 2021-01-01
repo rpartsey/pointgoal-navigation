0acf262c68ab44ce749bc8c4bba85215b505c4c5
rpartsey <partsey2412@gmail.com>
2021-01-01 14:18:59 +0200

    - defined VONetBaseModel - base visual odometry model; - implemented VONetResnet18 model.

--- odometry/model.py
+++ odometry/model.py
@@ -1,8 +1,10 @@
+from abc import ABC, abstractmethod
+
 import torch.nn as nn
 import torchvision.models as models
 
 
-class VONet(nn.Module):
+class VONetBaseModel(nn.Module, ABC):
     def __init__(self, encoder, fc):
         super().__init__()
         self.encoder = encoder
@@ -15,18 +17,40 @@ class VONet(nn.Module):
         return x
 
     @staticmethod
-    def create_fc_layers(in_features, p_dropout=0.0):
+    def create_fc_layers(input_dim: int, hidden_size: list, output_dim: int, p_dropout: float = 0.0):
+        hidden_size.insert(0, input_dim)
+
+        layers = []
+        for i in range(len(hidden_size)-1):
+            layers += [
+                nn.Dropout(p_dropout),
+                nn.Linear(hidden_size[i], hidden_size[i+1]),
+                nn.ReLU(True),
+            ]
+
         return nn.Sequential(
-            nn.Linear(in_features, 512),
+            nn.Flatten(),
+            *layers,
             nn.Dropout(p_dropout),
-            nn.ReLU(True),
-            nn.Linear(512, 4)
+            nn.Linear(hidden_size[-1], output_dim)
         )
 
     @classmethod
-    def from_torchvision_resnet18(cls):
+    @abstractmethod
+    def from_config(cls, config):
+        pass
+
+
+class VONetResnet18(VONetBaseModel):
+    @classmethod
+    def from_config(cls, config):
         resnet18 = models.resnet18()
-        fc = cls.create_fc_layers(resnet18.fc.in_features)
+        fc = cls.create_fc_layers(
+            input_dim=config.input_dim,
+            hidden_size=config.hidden_size,
+            output_dim=config.output_dim,
+            p_dropout=config.p_dropout
+        )
         del resnet18.fc
 
         return cls(resnet18, fc)
