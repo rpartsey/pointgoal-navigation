283d93c460f9915645b27e9b5c0442f4fdf04e93
rpartsey <partsey2412@gmail.com>
2021-04-10 15:19:28 +0300

    finished pointgoal_navigation_install.sh

--- pointgoal_navigation_install.sh
+++ pointgoal_navigation_install.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+# git clone git@github.com:rpartsey/pointgoal-navigation.git
+git checkout main
+
 conda create -n pointgoal-navigation-env python=3.6 cmake=3.14.0 -y
-source ~/miniconda3/etc/profile.d/conda.sh
+source ~/miniconda/etc/profile.d/conda.sh
 conda activate pointgoal-navigation-env
+
+#   navigate one directory up
+cd ..
 
 # ----------------------------------------------------------------------------
 # install habitat-sim
@@ -12,7 +18,6 @@ conda activate pointgoal-navigation-env
 
 conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
 conda clean -ya
-
 git clone https://github.com/facebookresearch/habitat-sim.git
 cd habitat-sim
 git checkout 856d4b08c1a2632626bf0d205bf46471a99502b7 # v0.1.7
@@ -50,26 +55,23 @@ python setup.py develop --all
 cd ..
 
 # ----------------------------------------------------------------------------
-#   install pointgoal-navigation
+#   install pointgoal-navigation requirements
 # ----------------------------------------------------------------------------
-
-git clone git@github.com:rpartsey/pointgoal-navigation.git
 cd pointgoal-navigation
-git checkout main
 
-# install requirements
 python -m pip install torch==1.7.1 torchvision==0.8.2 segmentation-models-pytorch==0.1.3
 
 # ----------------------------------------------------------------------------
 #   download the dataset for Gibson PointNav
 # ----------------------------------------------------------------------------
-
 python -m pip install gdown
 
 gdown https://dl.fbaipublicfiles.com/habitat/data/datasets/pointnav/gibson/v2/pointnav_gibson_v2.zip
 mkdir -p data/datasets/pointnav/gibson
 unzip pointnav_gibson_v2.zip -d data/datasets/pointnav/gibson
-#ln -s $PATH_TO_SCENE_DATASETS data/scene_datasets
 rm pointnav_gibson_v2.zip
-
 gdown https://drive.google.com/uc?id=15_vh9rZgNhk_B8RFWZqmcW5JRdNQKM2G --output data/datasets/pointnav/gibson/gibson_quality_ratings.csv
+
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+echo -e "\n${RED}NOTE:${NOCOLOR} use command 'ln -s <path to scene datasets> ${PWD}/data/scene_datasets' to link the simulation scenes.\n"
