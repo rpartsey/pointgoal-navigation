5f4157ec723ecb8b59d7962d86bac32606aedeb1
rpartsey <partsey2412@gmail.com>
2021-04-28 16:26:36 +0300

    added 'torch.backends.cudnn.benchmark = False' to set_random_seed

--- odometry/utils/utils.py
+++ odometry/utils/utils.py
@@ -10,6 +10,7 @@ def set_random_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def transform_batch(batch):
