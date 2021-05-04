fixed metrics averaging to produce correct results in distributed mode

train_odometry.py
@@ -59,6 +59,7 @@ def init_experiment(config):
 def train(model, optimizer, train_loader, loss_f, metric_fns, device):
     model.train()
 
+    num_items = 0
     metrics = defaultdict(lambda: 0)
 
     for data in tqdm(train_loader):
@@ -81,10 +82,10 @@ def train(model, optimizer, train_loader, loss_f, metric_fns, device):
             metrics[loss_component] += value.item() * batch_size
         for metric_f in metric_fns:
             metrics[metric_f.__name__] += metric_f(output, target).item() * batch_size
+        num_items += batch_size
 
-    dataset_length = len(train_loader.dataset)
     for metric_name in metrics:
-        metrics[metric_name] /= dataset_length
+        metrics[metric_name] /= num_items
 
     return metrics
 
@@ -92,6 +93,7 @@ def train(model, optimizer, train_loader, loss_f, metric_fns, device):
 def val(model, val_loader, loss_f, metric_fns, device):
     model.eval()
 
+    num_items = 0
     metrics = defaultdict(lambda: 0)
 
     with torch.no_grad():
@@ -111,10 +113,10 @@ def val(model, val_loader, loss_f, metric_fns, device):
                 metrics[loss_component] += value.item() * batch_size
             for metric_f in metric_fns:
                 metrics[metric_f.__name__] += metric_f(output, target).item() * batch_size
+            num_items += batch_size
 
-    dataset_length = len(val_loader.dataset)
     for metric_name in metrics:
-        metrics[metric_name] /= dataset_length
+        metrics[metric_name] /= num_items
 
     return metrics
 