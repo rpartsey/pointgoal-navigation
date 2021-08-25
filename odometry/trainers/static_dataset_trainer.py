51b6e742030baa043ded57afd44473a7f2bb8a26
rpartsey <partsey2412@gmail.com>
2021-08-25 09:23:59 +0300

    initial StaticDatasetTrainer and OnlineDatasetTrainer implementation

--- odometry/trainers/static_dataset_trainer.py
+++ odometry/trainers/static_dataset_trainer.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 from datetime import datetime
 
+import torch
 from tqdm import tqdm
+
 from habitat_baselines.rl.ddppo.algo.ddp_utils import rank0_only
 
 from .base_trainer import BaseTrainer
@@ -63,44 +65,68 @@ class StaticDatasetTrainer(BaseTrainer):
         return metrics
 
     def val_epoch(self):
-        pass
+        self.model.eval()
+
+        num_items = 0
+        num_items_per_action = defaultdict(lambda: 0)
+
+        metrics = defaultdict(lambda: 0)
+
+        with torch.no_grad():
+            for data in tqdm(self.val_loader, disable=self.is_distributed()):
+                data, embeddings, target = transform_batch(data)
+                data = data.float().to(self.device)
+                target = target.float().to(self.device)
+                for k, v in embeddings.items():
+                    embeddings[k] = v.to(self.device)
+
+                output = self.model(data, **embeddings)
+                loss, loss_components = self.loss_f(output, target)
+
+                batch_size = target.shape[0]
+                metrics['loss'] += loss.item() * batch_size
+                for loss_component, value in loss_components.items():
+                    metrics[loss_component] += value.item() * batch_size
+                for metric_f in self.val_metric_fns:
+                    metrics[metric_f.__name__] += metric_f(output, target).item() * batch_size
+                    if self.config.compute_metrics_per_action:
+                        for action_id in embeddings['action'].unique():
+                            action_name = action_id_to_action_name[action_id.item()]
+                            action_mask = embeddings['action'] == action_id
+                            action_metric_name = f'{metric_f.__name__}_{action_name}'
+                            num_action_items = action_mask.sum()
+
+                            metrics[action_metric_name] += metric_f(output[action_mask], target[action_mask]).item() * num_action_items
+                            num_items_per_action[action_metric_name] += num_action_items
+
+                num_items += batch_size
+
+        for metric_name in metrics:
+            metrics[metric_name] /= num_items_per_action.get(metric_name, num_items)
+
+        return metrics
 
     def loop(self):
-        # for epoch in range(1, self.config.epochs + 1):
-        #     if rank0_only():
-        #         print(f'{datetime.now()} Epoch {epoch}')
-        #
-        #     train_metrics = train(model, optimizer, train_loader, loss_f, train_metric_fns, device, is_distributed,
-        #                           config.compute_metrics_per_action)
-        #     if is_distributed:
-        #         train_metrics = coalesce_post_step(train_metrics, device)
-        #     write_metrics(epoch, train_metrics, train_writer)
-        #     print_metrics('Train', train_metrics)
-        #
-        #     val_metrics = val(model, val_loader, loss_f, val_metric_fns, device, is_distributed,
-        #                       config.compute_metrics_per_action)
-        #     if is_distributed:
-        #         val_metrics = coalesce_post_step(val_metrics, device)
-        #     write_metrics(epoch, val_metrics, val_writer)
-        #     print_metrics('Val', val_metrics)
-        #
-        #     if hasattr(config, 'train_val'):
-        #         train_val_metrics = val(model, train_val_loader, loss_f, train_val_metric_fns, device, is_distributed,
-        #                                 config.compute_metrics_per_action)
-        #         if is_distributed:
-        #             val_metrics = coalesce_post_step(val_metrics, device)
-        #         write_metrics(epoch, train_val_metrics, train_val_writer)
-        #         print_metrics('Train-val', train_val_metrics)
-        #
-        #     early_stopping(val_metrics['loss'])
-        #     if rank0_only() and config.model.save:  # and early_stopping.counter == 0:
-        #         best_checkpoint_path = config.model.best_checkpoint_path.replace('.pt', f'_{str(epoch).zfill(3)}e.pt')
-        #         torch.save(model.state_dict(), best_checkpoint_path)
-        #         print('Saved best model checkpoint to disk.')
-        #     if early_stopping.early_stop:
-        #         print(f'Early stopping after {epoch} epochs.')
-        #         break
-        #
-        #     if scheduler:
-        #         scheduler.step()
-        pass
+        for epoch in range(1, self.config.epochs + 1):
+            if rank0_only():
+                print(f'{datetime.now()} Epoch {epoch}')
+
+            train_metrics = self.train_epoch()
+            if self.is_distributed():
+                train_metrics = self.coalesce_post_step(train_metrics, self.device)
+            self.write_metrics(epoch, train_metrics, self.train_writer)
+            self.print_metrics('Train', train_metrics)
+
+            val_metrics = self.val_epoch()
+            if self.is_distributed():
+                val_metrics = self.coalesce_post_step(val_metrics, self.device)
+            self.write_metrics(epoch, val_metrics, self.val_writer)
+            self.print_metrics('Val', val_metrics)
+
+            if rank0_only() and self.config.model.save:
+                best_checkpoint_path = self.config.model.best_checkpoint_path.replace('.pt', f'_{str(epoch).zfill(3)}e.pt')
+                torch.save(self.model.state_dict(), best_checkpoint_path)
+                print('Saved model checkpoint on disk.')
+
+            if self.scheduler:
+                self.scheduler.step()
