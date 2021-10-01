096973c1142e0f0aa2e300c718c6cf2dbceaeb14
rpartsey <partsey2412@gmail.com>
2021-10-01 17:35:54 +0300

    added NavRLEnvSPLReward as a habitat_baselines.common.environments.NavRLEnv extension to set terminal reward to 2.5 * SPL; added 'ddppo_pointnav_2021_spl_reward.yaml' - ddppo 2021 config where ENV_NAME is set to NavRLEnvSPLReward

--- run_ddppo.py
+++ run_ddppo.py
@@ -1,4 +1,5 @@
 import habitat_extensions.sensors  # noqa - required to register new sensors to baseline_registry
+import habitat_baselines_extensions  # noqa - required to register custom habitat baselines extensions
 from habitat_baselines.run import main
 
 
